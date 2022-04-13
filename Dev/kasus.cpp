9d45e04a145a25435a19c6bf5c9c57584b54edb3
Sinyster03 <mohammad2100018354@webmail.uad.ac.id>
2022-04-14 05:59:41 +0700

    file tabungan

---
+++ Dev/kasus.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include "../library/bagian1.h"
+#include "../library/bagian2.h"
+int main (){
+	Tabungan nanda;
+	nanda.tabungan;
+	nanda.cetak();
+	return 0;
+}
